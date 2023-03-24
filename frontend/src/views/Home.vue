ec11e6706e313edfc11239eb706cfd43950f94b4
ilovemhuukrob <78673905+ilovemhuukrob@users.noreply.github.com>
2023-03-24 19:56:17 +0700

    eslint disable

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,7 +1,8 @@
 <template>
-    <LeftNavigation  :userDetail='userDetail'></LeftNavigation>
-    <router-view :data='dynamicData'></router-view>
-    <RightProfile   :userDetail='userDetail'></RightProfile>
+  <LeftNavigation :userDetail="userDetail"></LeftNavigation>
+  <router-view :data="dynamicData"></router-view>
+  <!-- eslint-disable -->
+  <RightProfile :userDetail="userDetail"></RightProfile>
 </template>
 
 <script setup >
@@ -11,41 +12,40 @@ import HomeCategories from "@/components/HomeCategories.vue";
 import HomeRestaurantList from "@/layouts/default/HomeRestaurantList.vue";
 import LeftNavigation from "@/components/LeftNavigation.vue";
 import RightProfile from "@/components/RightProfile.vue";
-
-
 </script>
 <script >
-   import axios from "axios"
-    export default {
-     data: ()=>({
-       refresh_token: '',
-       access_token : '',
-       nameUser: '',
-       userDetail : {},
-       name : '',
-       dynamicData :[]
-     }),
-     beforeCreate(){
-       // get refresh token and access toke
-       console.log("beforeCreate-at-HomePage")
-       this.refresh_token = localStorage.getItem('refresh_token');
-       this.access_token =  localStorage.getItem('access_token');
-       axios.get('http://localhost:5000/api/Home',{
-         headers:{
-           Authorization: 'Bearer ' + this.access_token
-         }
-       }).then(
-         (res) =>{
-            this.userDetail = res.data.users[0]
-            this.dynamicData = res.data.restuarants
-            this.dynamicData = JSON.parse(JSON.stringify(this.dynamicData  ))
-            console.log(this.dynamicData)
-            
-            
+import axios from "axios";
+export default {
+  data: () => ({
+    refresh_token: "",
+    access_token: "",
+    nameUser: "",
+    userDetail: {},
+    name: "",
+    dynamicData: [],
+  }),
+  beforeCreate() {
+    // get refresh token and access toke
+    console.log("beforeCreate-at-HomePage");
+    this.refresh_token = localStorage.getItem("refresh_token");
+    this.access_token = localStorage.getItem("access_token");
+    axios
+      .get("http://localhost:5000/api/Home", {
+        headers: {
+          Authorization: "Bearer " + this.access_token,
         },
-        (res) =>{
-          this.$router.push("/")
+      })
+      .then(
+        (res) => {
+          this.userDetail = res.data.users[0];
+          this.dynamicData = res.data.restuarants;
+          this.dynamicData = JSON.parse(JSON.stringify(this.dynamicData));
+          console.log(this.dynamicData);
+        },
+        (res) => {
+          this.$router.push("/");
         }
-      )},   
-  }
+      );
+  },
+};
 </script>
