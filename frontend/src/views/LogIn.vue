e2db85a2a69d271d0a811b437bc992f6cf417277
ilovemhuukrob <78673905+ilovemhuukrob@users.noreply.github.com>
2023-03-24 19:49:53 +0700

    แก้ ui หน้า login

--- frontend/src/views/LogIn.vue
+++ frontend/src/views/LogIn.vue
@@ -1,155 +1,209 @@
 <template>
   <v-sheet color="#2255A4">
-    <v-card class="pa-8 elevation-5" rounded="xl">
-      <v-img contain width="180" src="https://cdn.discordapp.com/attachments/1019966926014910516/1085258301572386928/itfoodhub.png" />
-      <p class="text-center ma-5">To continue, log in to IT Food Hub.</p>
-      <p class="text-center ma-5 response" color="danger" >{{this.response}}</p>
-      <v-responsive width="400px" v-if="Login">
+    <v-card class="pa-16 elevation-5" rounded="xl">
+      <v-img
+        contain
+        width="180"
+        src="https://cdn.discordapp.com/attachments/1019966926014910516/1085258301572386928/itfoodhub.png"
+      />
+      <v-divider class="w-100 ma-7"></v-divider>
+      <span class="w-100" v-if="Login">
+        <p class="text-center" style="font-weight: 600">
+          To continue, log in to IT Food Hub.
+        </p>
+
+        <v-btn class="w-100 mt-5" size="large" rounded="pill">
+          <v-img
+            :width="20"
+            aspect-ratio="1"
+            src="https://cdn-icons-png.flaticon.com/512/2991/2991148.png"
+          ></v-img>
+          <div class="px-2"></div>
+          <p>CONTINUE WITH GOOGLE</p>
+        </v-btn>
+
+        <div class="w-100 d-flex align-center">
+          <v-divider></v-divider>
+          <p class="ma-5" style="font-weight: 600">OR</p>
+          <v-divider></v-divider>
+        </div>
+      </span>
+      <p class="text-center response" color="danger">{{ this.response }}</p>
+      <v-responsive class="w-100" v-if="Login">
         <v-form fast-fail @submit.prevent ref="form">
-          <v-text-field 
+          <v-text-field
             v-model="email"
             :rules="[rules.required, rules.email]"
             label="email"
-          
           ></v-text-field>
           <v-text-field
             v-model="password"
             label="password"
             :rules="[rules.required, rules.counter]"
           ></v-text-field>
-          <v-col>
-              <v-btn type="submit" block class="mt-1 mb-4" @click="Loginsubmit">LOG IN</v-btn>
-              <v-btn type="submit" block class="mt-1 mb-4" @click=" Login =!Login" >SIGN UP</v-btn>
-          </v-col>
+          <v-btn
+            type="submit"
+            block
+            color="it"
+            size="large"
+            rounded="pill"
+            @click="Loginsubmit"
+            >LOG IN</v-btn
+          >
+          <v-divider class="my-5"></v-divider>
+          <p class="text-center" style="font-weight: 600">
+            Don't have an account?
+          </p>
+          <v-btn
+            type="submit"
+            block
+            class="my-5"
+            size="large"
+            rounded="pill"
+            @click="Login = !Login"
+            >SIGN UP</v-btn
+          >
         </v-form>
-       </v-responsive>
-       <v-responsive width="400px" v-if="!Login"   >
-        <v-form fast-fail @submit.prevent ref="form"> 
-            <v-text-field
-              v-model="firstName"
-              color="primary"
-              label="First name"
-              variant="underlined"
-              placeholder="Enter your first name"
-              :rules="[rules.required, rules.counter]"
-            ></v-text-field>
-      
-            <v-text-field
-              v-model="lastName"
-              color="primary"
-              label="Last name"
-              variant="underlined"
-              placeholder="Enter your lastname"
-              :rules="[rules.required, rules.counter]"
-            ></v-text-field>
-    
-            <v-text-field
-              v-model="email"
-              :rules="[rules.required, rules.email]"
-              color="primary"
-              label="Email"
-              placeholder="Enter your email address"
-              variant="underlined"
-            ></v-text-field>
-      
-            <v-text-field
-              v-model="password"
-              color="primary"
-              label="Password"
-              placeholder="Enter your password"
-              variant="underlined"
-              :rules="[rules.required, rules.counter]"
-            ></v-text-field>
-            <v-col>
-              <v-btn type="submit" block class="mt-1 mb-4" @click=" Login =!Login">ALREADY HAVE ACCOUNT</v-btn>
-              <v-btn type="submit" block class="mt-1 mb-4" @click="Login =!Login" >Register</v-btn>
-          </v-col>
+      </v-responsive>
+      <v-responsive class="w-100" v-if="!Login">
+        <v-form fast-fail @submit.prevent ref="form">
+          <v-text-field
+            v-model="firstName"
+            color="primary"
+            label="First name"
+            variant="underlined"
+            placeholder="Enter your first name"
+            :rules="[rules.required, rules.counter]"
+          ></v-text-field>
+
+          <v-text-field
+            v-model="lastName"
+            color="primary"
+            label="Last name"
+            variant="underlined"
+            placeholder="Enter your lastname"
+            :rules="[rules.required, rules.counter]"
+          ></v-text-field>
+
+          <v-text-field
+            v-model="email"
+            :rules="[rules.required, rules.email]"
+            color="primary"
+            label="Email"
+            placeholder="Enter your email address"
+            variant="underlined"
+          ></v-text-field>
+
+          <v-text-field
+            v-model="password"
+            color="primary"
+            label="Password"
+            placeholder="Enter your password"
+            variant="underlined"
+            :rules="[rules.required, rules.counter]"
+          ></v-text-field>
+          <v-btn
+            type="submit"
+            block
+            class="mt-1 mb-4"
+            @click="Login = !Login"
+            size="large"
+            rounded="pill"
+            >ALREADY HAVE ACCOUNT</v-btn
+          >
+          <v-btn
+            type="submit"
+            block
+            class="mt-1 mb-4"
+            @click="Login = !Login"
+            size="large"
+            rounded="pill"
+            >Register</v-btn
+          >
         </v-form>
-       </v-responsive>
-      <v-btn rounded="pill">
-        <v-img
-          :width="20"
-          aspect-ratio="1"
-          src="https://cdn-icons-png.flaticon.com/512/2991/2991148.png"
-        ></v-img>
-        <div class="px-2"></div>
-        <p>CONTINUE WITH GOOGLE</p>
-        </v-btn>
+      </v-responsive>
     </v-card>
   </v-sheet>
 </template>
+
 <script>
-  import axios from "axios"
-  export default {
-    data : ()=> ({
-      valid: true,
-      Login: true,
-      firstName: '',
-      lastName :'',
-      email: '',
-      formHasErrors: false,
-      password : '',
-      response : '',
-      userDetail :{},
-      
-      rules: {
-          // requried เช็คว่า value มีมั้ย
-          required: value =>  {if(!!value ) return true
-            return 'Required.'},
-          counter: value => { if (value.length <= 20 && value.length >=5) return true  
-              return 'Max 20 characters and Length must be less than 5'}  ,
-          email: value => {
-            const pattern = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-            return pattern.test(value) || 'Invalid e-mail.'
-          }
-    }
-  }),
-  computed :{
-    form () {
-        return {
-          firstName: this.firstName,
-          lastName: this.lastName,
-          password: this.password,
-          email: this.email,
-        }
+import axios from "axios";
+export default {
+  data: () => ({
+    valid: true,
+    Login: true,
+    firstName: "",
+    lastName: "",
+    email: "",
+    formHasErrors: false,
+    password: "",
+    response: "",
+    userDetail: {},
+
+    rules: {
+      // requried เช็คว่า value มีมั้ย
+      required: (value) => {
+        if (!!value) return true;
+        return "Required.";
       },
+      counter: (value) => {
+        if (value.length <= 20 && value.length >= 5) return true;
+        return "Max 20 characters and Length must be less than 5";
+      },
+      email: (value) => {
+        const pattern =
+          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return pattern.test(value) || "Invalid e-mail.";
+      },
+    },
+  }),
+  computed: {
+    form() {
+      return {
+        firstName: this.firstName,
+        lastName: this.lastName,
+        password: this.password,
+        email: this.email,
+      };
+    },
   },
-  methods :{
+  methods: {
     //LOGIN
-    async Loginsubmit () {
+    async Loginsubmit() {
       const loginDetails = {
         email: this.email,
-        password : this.password
-      }
-      const { valid } =  await this.$refs.form.validate()
-      if(valid){
-        this.response =''
-        console.log(loginDetails)
-        axios.post('http://localhost:5000/api/login', loginDetails)
-        .then(
-          (res)=>{
+        password: this.password,
+      };
+      const { valid } = await this.$refs.form.validate();
+      if (valid) {
+        this.response = "";
+        console.log(loginDetails);
+        axios.post("http://localhost:5000/api/login", loginDetails).then(
+          (res) => {
             // ถ้า login ผ่าน
-            if(res.data)
-              {
-                console.log(res.data)
-                this.userDetail = res.data
-                window.localStorage.setItem("access_token", res.data.accessToken)
-                window.localStorage.setItem("refresh_token", res.data. refreshToken)
-                this.$router.push("/Home")
-              }
+            if (res.data) {
+              console.log(res.data);
+              this.userDetail = res.data;
+              window.localStorage.setItem("access_token", res.data.accessToken);
+              window.localStorage.setItem(
+                "refresh_token",
+                res.data.refreshToken
+              );
+              this.$router.push("/Home");
+            }
           },
-          (res)=>{
+          (res) => {
             // ถ้าlog in ไม่ถูกต้องจะขึ้นแบบนี้
-            if(res.response.data != undefined ){
-              console.log(res.response.data)
-              this.response = res.response.data
+            if (res.response.data != undefined) {
+              console.log(res.response.data);
+              this.response = res.response.data;
             }
           }
-        )
-      }},
-  }
-}
-
+        );
+      }
+    },
+  },
+};
 </script> 
   impoer 
 
@@ -160,10 +214,10 @@
   display: flex;
   justify-content: center;
   align-items: center;
-  background-image: url('/src/assets/bannerpattern3.png');
+  background-image: url("/src/assets/bannerpattern3.png");
   background-position: center;
 }
-.response{
+.response {
   color: red;
 }
 .v-card {
