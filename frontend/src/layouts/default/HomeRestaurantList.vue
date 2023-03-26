da3e4620717c3445f1d559e135be9c42fd62abc6
ilovemhuukrob <78673905+ilovemhuukrob@users.noreply.github.com>
2023-03-26 13:00:23 +0700

    move routerlink

--- frontend/src/layouts/default/HomeRestaurantList.vue
+++ frontend/src/layouts/default/HomeRestaurantList.vue
@@ -6,55 +6,54 @@
         :key="i"
         class="d-flex flex-column justify-center my-4"
         cols="3"
-      >
-        <v-card
-          style="background: none"
-          class="elevation-0"
-          rounded="xl"
-          v-ripple
-        >
-          <router-link :to="'/itfoodhub/' + restaurant.Restaurant_name">
-          <v-card class="restaurant-img ma-auto">
-            <v-img
-              cover
-              height="180"
-              aspect-ratio="16/9"
-              src="https://cf.shopee.co.th/file/5160e1eb7f9b68c33e15e06780dac2ca"
-            >
-            </v-img>
-          </v-card>
+        ><router-link :to="'/itfoodhub/' + restaurant.Restaurant_name">
           <v-card
+            style="background: none"
+            class="elevation-0"
             rounded="xl"
-            color="#2255A4"
-            class="restaurant-content pt-16 pb-2"
+            v-ripple
           >
-            <v-card-item>
-              <v-card-title
-                class="py-1 d-flex justify-space-between"
-                style="color: #e3b54c"
+            <v-card class="restaurant-img ma-auto">
+              <v-img
+                cover
+                height="180"
+                aspect-ratio="16/9"
+                src="https://cf.shopee.co.th/file/5160e1eb7f9b68c33e15e06780dac2ca"
               >
-                <p class="h2">{{ restaurant.Restaurant_name }}</p>
-                <p
-                  class="px-2 elevation-5"
-                  style="
-                    font-size: 1rem;
-                    color: white;
-                    background: #36c144;
-                    border-radius: 1rem;
-                  "
+              </v-img>
+            </v-card>
+            <v-card
+              rounded="xl"
+              color="#2255A4"
+              class="restaurant-content pt-16 pb-2"
+            >
+              <v-card-item>
+                <v-card-title
+                  class="py-1 d-flex justify-space-between"
+                  style="color: #e3b54c"
                 >
-                  {{ restaurant.status }}
-                </p>
-              </v-card-title>
-              <v-card-subtitle
-                ><p style="color: #fff">
-                  {{ restaurant.Restuarant_type }}
-                </p></v-card-subtitle
-              >
-            </v-card-item>
-          </v-card>
-          </router-link>
-        </v-card>
+                  <p class="h2">{{ restaurant.Restaurant_name }}</p>
+                  <p
+                    class="px-2 elevation-5"
+                    style="
+                      font-size: 1rem;
+                      color: white;
+                      background: #36c144;
+                      border-radius: 1rem;
+                    "
+                  >
+                    {{ restaurant.status }}
+                  </p>
+                </v-card-title>
+                <v-card-subtitle
+                  ><p style="color: #fff">
+                    {{ restaurant.Restuarant_type }}
+                  </p></v-card-subtitle
+                >
+              </v-card-item>
+            </v-card>
+          </v-card></router-link
+        >
       </v-col>
     </v-row>
   </v-container>
@@ -76,7 +75,6 @@
 </style>
 
 <script>
-import axios from "axios";
 export default {
   data: () => ({
     test: "",
