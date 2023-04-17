c674b977705db5f766dc7f4a050ff336da10e3ea
ilovemhuukrob <78673905+ilovemhuukrob@users.noreply.github.com>
2023-04-18 00:07:21 +0700

    no message

--- server/middleware/requireRole.js
+++ server/middleware/requireRole.js
@@ -1,6 +1,6 @@
 const requireRole = (role) => {
   return (req, res, next) => {
-    if (req.user && req.user.role === role) {
+    if (req.user && req.user.user_role === role) {
       next();
     } else {
       res.status(401).json({ message: "Unauthorized" });
