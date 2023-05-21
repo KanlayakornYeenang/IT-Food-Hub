7274069c2ad133618941dc98141a475002733af3
ilovemhuukrob <78673905+ilovemhuukrob@users.noreply.github.com>
2023-05-22 05:12:03 +0700

    fix conflict

--- server/controllers/users/user.controller.js
+++ server/controllers/users/user.controller.js
@@ -1,9 +1,6 @@
-const jwt = require("jsonwebtoken");
 const {
-  getUserByNameAndPasword,
   getUserDetailById,
   registerUser,
-  getAllUser,
   updatePassword,
   insertProfilePicture,
   updateRoleUser,
@@ -13,40 +10,6 @@ const {
   insertResPicture,
   selectIdRest
 } = require("../../models/users");
-
-
-const login = async (req, res) => {
-  try {
-    const { email, password } = req.body;
-    const result = await getUserByNameAndPasword(email, password);
-
-    if (result) {
-      const user = result;
-      const token = jwt.sign(
-        {
-          user_id: user.user_id,
-          user_role: user.user_role,
-          user_email: user.user_email,
-        },
-        "mySecretKey",
-        {
-          expiresIn: "1d",
-        }
-      );
-      return res.json({
-        success: true,
-        message: "Login successful",
-        token,
-        user_detail: user,
-      });
-    }
-    return res
-      .status(401)
-      .json({ success: false, message: "Invalid email or password" });
-  } catch (error) {
-    res.status(500).send(error);
-  }
-};
 
 const getDetail = async (req, res) => {
   try {
@@ -240,7 +203,6 @@ const registerOfUser = async (req, res) => {
 };
 
 module.exports = {
-  login,
   getDetail,
   registerOfUser,
   changePasswordUser,
